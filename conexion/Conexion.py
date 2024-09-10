9a92f5f5dc2d983acee48a5edffcb10c5b3582ba
Thobias01 <thobias235@gmail.com>
2024-09-09 22:40:37 -0400

    segundo commit

--- conexion/Conexion.py
+++ conexion/Conexion.py
@@ -5,7 +5,7 @@ class Conexion:
     """Metodo constructor
     """
     def __init__(self):
-        self.con = psycopg2.connect("dbname=examenfinalbd user=postgres host=localhost password=19790523")
+        self.con = psycopg2.connect("dbname=examenfinaldb user=postgres host=localhost password=19790523")
 
     """getConexion
 
